9ebc14d7d82ae6e1309eff3b139d7a38722faccc
Troy <hello@troylusty.com>
2025-02-27 13:29:24 +0000

    feat: remove google font

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ function noSearchDefaultPageRender() {
           <input 
             type="text" 
             class="url-input"
-            value="https://unduck.link?q=%s"
+            value="https://unduck.troylusty.com?q=%s"
             readonly 
           />
           <button class="copy-button">
@@ -21,11 +21,10 @@ function noSearchDefaultPageRender() {
         </div>
       </div>
       <footer class="footer">
-        <a href="https://t3.chat" target="_blank">t3.chat</a>
+        <a href="https://github.com/troylusty/unduck" target="_blank">github</a>
         •
-        <a href="https://x.com/theo" target="_blank">theo</a>
-        •
-        <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
+        forked from
+        <a href="https://github.com/t3dotgg/unduck" target="_blank">t3dotgg/unduck</a>
       </footer>
     </div>
   `;
